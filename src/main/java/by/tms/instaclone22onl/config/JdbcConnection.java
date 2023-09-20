ad631bfa8ab47c78551d3345a25982885bb48ed0
Ilya Moiseenko <markalfox@gmail.com>
2023-09-20 19:41:52 +0300

    add: method for getting JdbcConnection

---
+++ src/main/java/by/tms/instaclone22onl/config/JdbcConnection.java
@@ -0,0 +1,26 @@
+package by.tms.instaclone22onl.config;
+
+/*
+    @author Ilya Moiseenko on 20.09.23
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/Instagram";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection;
+
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
+}
