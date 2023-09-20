0c381465eacd2a1352f5987ff9a3abe332b3d4bf
Ilya Moiseenko <markalfox@gmail.com>
2023-09-20 19:03:56 +0300

    add: ApplicationListener

---
+++ src/main/java/by/tms/instaclone22onl/web/servlet/listener/ApplicationListener.java
@@ -0,0 +1,21 @@
+package by.tms.instaclone22onl.web.servlet.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/*
+    @author Ilya Moiseenko on 20.09.23
+*/
+@WebListener
+public class ApplicationListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException();
+        }
+    }
+}
