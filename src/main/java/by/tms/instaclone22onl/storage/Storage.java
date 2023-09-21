e5c2e65aa73050e60f4fa2689a3761f330f66d56
Ivan <ivandolgolaptev@gmail.com>
2023-09-21 19:08:55 +0300

    init: add City Storage

--- src/main/java/by/tms/instaclone22onl/storage/Storage.java
+++
@@ -1,9 +0,0 @@
-package by.tms.instaclone22onl.storage;
-
-import by.tms.instaclone22onl.model.City;
-import java.util.Optional;
-
-public interface Storage {
-    Optional <City> getId(int id);
-    Optional <City> getName(String name);
-}
