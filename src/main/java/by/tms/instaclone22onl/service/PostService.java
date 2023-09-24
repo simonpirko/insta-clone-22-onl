70b438bf5d5d2b460ac3d306ed3fa16af37a82c8
Артем <375298702138artiom@gmail.com>
2023-09-24 22:22:40 +0300

    Added PostService

---
+++ src/main/java/by/tms/instaclone22onl/service/PostService.java
@@ -0,0 +1,41 @@
+package by.tms.instaclone22onl.service;
+
+import by.tms.instaclone22onl.model.Post;
+import by.tms.instaclone22onl.model.User;
+import by.tms.instaclone22onl.storage.PostStorage.JdbcPostStorage;
+import by.tms.instaclone22onl.storage.PostStorage.PostStorage;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PostService {
+
+    private final PostStorage postStorage = new JdbcPostStorage();
+
+    public void addPost(Post post){
+        postStorage.addPost(post);
+    }
+    public Optional<Post> getPost(int id){
+        return postStorage.getPost(id);
+    }
+
+    public Optional<Post> getPost(User user){
+        return postStorage.getPost(user);
+    }
+
+    public List<Post> getAllPost(){
+        return postStorage.getAllPost();
+    }
+
+    public boolean deletePost(int id){
+        return postStorage.deletePost(id);
+    }
+
+    public boolean deletePost(User user){
+        return postStorage.deletePost(user);
+    }
+
+    public void updatePost(int id, Post newPost){
+        postStorage.updatePost(id, newPost);
+    }
+}
