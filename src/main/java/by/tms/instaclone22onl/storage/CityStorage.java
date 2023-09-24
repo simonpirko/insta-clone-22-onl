d4a4f7949b581e5734120de0e6b7eb0f4172b9a5
Ilya Moiseenko <markalfox@gmail.com>
2023-09-24 20:41:25 +0300

    remove storage

--- src/main/java/by/tms/instaclone22onl/storage/CityStorage.java
+++
@@ -1,9 +0,0 @@
-package by.tms.instaclone22onl.storage;
-
-import by.tms.instaclone22onl.model.City;
-import java.util.Optional;
-
-public interface CityStorage {
-    Optional <City> getById(int id);
-    Optional <City> getByName(String name);
-}
