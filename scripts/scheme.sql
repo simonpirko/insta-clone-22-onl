2399892e11ccb4f8b9f772fb697b41f5fe10b510
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-09-24 20:11:03 +0300

    Revert changes in scheme.sql

--- scripts/scheme.sql
+++ scripts/scheme.sql
@@ -1,34 +1,34 @@
 create table if not exists "country" (
                                          id serial not null primary key,
                                          name varchar(50) not null unique
-    );
+);
 
 create table if not exists "human" (
                                        id serial not null primary key,
                                        name varchar(50) not null,
-    surname varchar(50) not null,
-    username varchar(20) not null unique,
-    photo bytea,
-    email varchar(50) not null unique,
-    password varchar(50) not null,
-    country_id int not null,
-    foreign key (country_id) references "country"(id)
-    );
+                                       surname varchar(50) not null,
+                                       username varchar(20) not null unique,
+                                       photo bytea,
+                                       email varchar(50) not null unique,
+                                       password varchar(50) not null,
+                                       country_id int not null,
+                                       foreign key (country_id) references "country"(id)
+);
 
 create table if not exists "city" (
                                       id serial not null primary key,
                                       name varchar(50) not null unique,
-    country_id int,
-    foreign key (country_id) references "country"(id)
-    );
+                                      country_id int,
+                                      foreign key (country_id) references "country"(id)
+);
 
 create table if not exists "post" (
                                       id serial not null primary key,
                                       author_id int,
                                       photo bytea,
                                       description varchar(250),
-    foreign key (author_id) references "human"(id)
-    );
+                                      foreign key (author_id) references "human"(id)
+);
 
 CREATE TABLE "post_like"(
                             author_id int REFERENCES "human"(id),
@@ -37,7 +37,7 @@ CREATE TABLE "post_like"(
 );
 
 create table if not exists "comment" (
-    author_id int references "human"(id),
-    post_id int references "post"(id),
-    constraint comment_pk primary key (author_id, post_id)
-    );
+                                         author_id int references "human"(id),
+                                         post_id int references "post"(id),
+                                         text varchar(255) not null
+);
