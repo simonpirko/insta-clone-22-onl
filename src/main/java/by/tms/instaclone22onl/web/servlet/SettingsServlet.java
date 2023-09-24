18042ff360719f64b1c98af732f4cc4247ddcd18
Ivan <ivandolgolaptev@gmail.com>
2023-09-25 01:43:06 +0300

    init: add new branch INST-25_Add_a_customization_page

--- src/main/java/by/tms/instaclone22onl/web/servlet/SettingsServlet.java
+++ src/main/java/by/tms/instaclone22onl/web/servlet/SettingsServlet.java
@@ -1,61 +1,35 @@
 package by.tms.instaclone22onl.web.servlet;
 
-import by.tms.instaclone22onl.config.JdbcConnection;
-import by.tms.instaclone22onl.model.User;
-import by.tms.instaclone22onl.service.UserService;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.sql.*;
-import java.util.Base64;
-import java.util.Optional;
 
 @WebServlet("/settings")
 public class SettingsServlet extends HttpServlet {
-    private final UserService service = UserService.getInstance();
-    private User user;
-    private final String UPDATE_USER_DATA = "UPDATE \"human\" SET name = ?, surname = ?, username = ?,  email = ?, password = ?\n" +
-                                            "WHERE id = ?";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       /* if (req.getSession().getAttribute("") == null){
+        if (req.getSession().getAttribute("currentUser") == null){
             resp.sendRedirect(req.getContextPath() + "/pages/login.jsp");
-        } else {}*/
+        } else {
             getServletContext().getRequestDispatcher("/pages/settings.jsp").forward(req, resp);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("name");
+        String secondName = req.getParameter("surname");
+        String userName = req.getParameter("username");
+        String photo = req.getParameter("photo");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
 
-
-       /* if (req.getSession().getAttribute("currentUser") == null){
+        if (req.getSession().getAttribute("currentUser") == null){
             resp.sendRedirect(req.getContextPath() + "/pages/login.jsp");
-        } else {}*/
-
-            try(Connection connection = JdbcConnection.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_DATA)) {
-
-                preparedStatement.setString(1, user.getName());
-                preparedStatement.setString(2, user.getSurname());
-                preparedStatement.setString(3, user.getUsername());
-                preparedStatement.setString(4, user.getEmail());
-                preparedStatement.setString(5, user.getPassword());
-                preparedStatement.setInt(6, user.getId());
-
-                int rowsAffected = preparedStatement.executeUpdate();
-
-                if (rowsAffected > 0) {
-                    resp.sendRedirect(req.getContextPath() + "/pages/settings.jsp");
-                } else {
-                    resp.sendRedirect(req.getContextPath() + "/pages/error.jsp");
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+        } else {
+        }
     }
 }
