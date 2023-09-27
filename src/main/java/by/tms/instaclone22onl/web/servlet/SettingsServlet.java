5f331e38190ecbe434ef7e8864f7913904f0b1cd
Ivan <ivandolgolaptev@gmail.com>
2023-09-28 00:02:39 +0300

    fix: SettingsServlet

--- src/main/java/by/tms/instaclone22onl/web/servlet/SettingsServlet.java
+++ src/main/java/by/tms/instaclone22onl/web/servlet/SettingsServlet.java
@@ -1,7 +1,7 @@
 package by.tms.instaclone22onl.web.servlet;
 
-import by.tms.instaclone22onl.config.JdbcConnection;
 import by.tms.instaclone22onl.model.User;
+import by.tms.instaclone22onl.storage.UserStorage.JdbcUserStorage;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,11 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.*;
 @WebServlet("/settings")
 public class SettingsServlet extends HttpServlet {
-    private final String UPDATE_USER_DATA = "UPDATE \"human\" SET name = ?, surname = ?, username = ?,  email = ?, password = ?\n" +
-                                            "WHERE id = ?";
+   private JdbcUserStorage storage;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        if (req.getSession().getAttribute("currentUser") == null){
@@ -30,38 +28,31 @@ public class SettingsServlet extends HttpServlet {
         String name = req.getParameter("name");
         String surname = req.getParameter("surname");
         String username = req.getParameter("username");
+        String photo = req.getParameter("photo");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
+        int userId = Integer.parseInt(req.getParameter("id"));
 
         currentUser.setName(name);
         currentUser.setSurname(surname);
         currentUser.setUsername(username);
+        currentUser.setPhoto(photo);
         currentUser.setUsername(email);
         currentUser.setPassword(password);
+        currentUser.setId(userId);
 
        if (req.getSession().getAttribute("currentUser") == null){
             resp.sendRedirect("/pages/login.jsp");
         } else {
-           try(Connection connection = JdbcConnection.getConnection();
-               PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_DATA)) {
+          try {
 
-               preparedStatement.setString(1, currentUser.getName());
-               preparedStatement.setString(2, currentUser.getSurname());
-               preparedStatement.setString(3, currentUser.getUsername());
-               preparedStatement.setString(4, currentUser.getEmail());
-               preparedStatement.setString(5, currentUser.getPassword());
-               preparedStatement.setInt(6, currentUser.getId());
+              storage.updateUserCountry(currentUser.getId(), String.valueOf(currentUser.getCountry()));
+              storage.updateById(currentUser);
 
-               int rowsAffected = preparedStatement.executeUpdate();
-
-               if (rowsAffected > 0) {
-                   resp.sendRedirect("/pages/settings.jsp");
-               } else {
-                   resp.sendRedirect("/pages/error.jsp");
-               }
-           } catch (SQLException e) {
-               throw new RuntimeException(e);
-           }
+              resp.sendRedirect("/pages/settings.jsp");
+          } catch (IOException e) {
+              throw new RuntimeException(e);
+          }
        }
     }
 }
