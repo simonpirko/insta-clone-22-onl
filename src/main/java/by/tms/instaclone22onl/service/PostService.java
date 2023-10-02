67ccb442808f791adc9518e330ad76dc7c72bac8
Ilya Moiseenko <ilya.moiseenko.00@yandex.by>
2023-10-02 21:10:22 +0300

    fix: setting up a post in the PostService

--- src/main/java/by/tms/instaclone22onl/service/PostService.java
+++ src/main/java/by/tms/instaclone22onl/service/PostService.java
@@ -1,7 +1,11 @@
 package by.tms.instaclone22onl.service;
 
+import by.tms.instaclone22onl.model.Comment;
+import by.tms.instaclone22onl.model.Like;
 import by.tms.instaclone22onl.model.Post;
 import by.tms.instaclone22onl.model.User;
+import by.tms.instaclone22onl.storage.CommentStorage.CommentStorage;
+import by.tms.instaclone22onl.storage.CommentStorage.JdbcCommentStorage;
 import by.tms.instaclone22onl.storage.PostStorage.JdbcPostStorage;
 import by.tms.instaclone22onl.storage.PostStorage.PostStorage;
 
@@ -13,6 +17,8 @@ public class PostService {
     private static PostService instance;
 
     private final PostStorage postStorage = JdbcPostStorage.getInstance();
+    private final CommentStorage commentStorage = JdbcCommentStorage.getInstance();
+    private final LikeService likeService = LikeService.getInstance();
 
     private PostService() {}
 
@@ -26,8 +32,26 @@ public class PostService {
     public void addPost(Post post){
         postStorage.addPost(post);
     }
-    public Optional<Post> getPost(int id){
-        return postStorage.getPost(id);
+    public Optional<Post> getPost(int id) {
+        Optional<Post> optionalPost = postStorage.getPost(id);
+
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+
+            Optional<List<Comment>> commentsByPost = commentStorage.getByPost(post);
+            List<Like> allLikesByPost = likeService.findAllByPost(post);
+
+            if (commentsByPost.isPresent()) {
+                List<Comment> comments = commentsByPost.get();
+
+                post.setComments(comments);
+                post.setLikes(allLikesByPost);
+            }
+
+            return Optional.of(post);
+        }
+
+        return optionalPost;
     }
 
     public Optional<Post> getPost(User user){
