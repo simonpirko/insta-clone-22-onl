d47aeca245159c26c5bb4833f005053186858b8e
Ivan <ivandolgolaptev@gmail.com>
2023-10-03 05:05:23 +0300

    init: add new branch INST-25_Add_a_customization_page
     Conflicts:
    	src/main/java/by/tms/instaclone22onl/web/servlet/SettingsServlet.java
    	src/main/webapp/pages/settings.jsp

--- src/main/java/by/tms/instaclone22onl/storage/UserStorage/JdbcUserStorage.java
+++ src/main/java/by/tms/instaclone22onl/storage/UserStorage/JdbcUserStorage.java
@@ -15,9 +15,14 @@ public class JdbcUserStorage implements UserStorage {
 
     private static JdbcUserStorage instance;
 
-    private final String INSERT = "insert into \"human\" (name, surname, username, photo, email, password, countryId) values (?, ?, ?, ?, ?, ?, ?)";
+    private final String INSERT = "insert into \"human\" (name, surname, username, photo, email, password, country_id) values (?, ?, ?, ?, ?, ?, ?)";
     private final String GET_BY_ID_WITH_COUNTRY = "select * from \"human\" join \"country\" on \"human\".country_id = \"country\".id where \"human\".id = ?";
     private final String GET_BY_USERNAME_WITH_COUNTRY = "select * from \"human\" join \"country\" on \"human\".country_id = \"country\".id where \"human\".username = ?";
+    private final String UPDATE_USER_DATA = "UPDATE human SET name = ?, surname = ?, username = ?, photo = ?, email = ?, password = ?, country_id = ?\n" +
+                                            "FROM country\n" +
+                                            "WHERE human.id = country.id\n" +
+                                            "AND human.id = ?;";
+    private final String UPDATE_USER_COUNTRY = "UPDATE \"human\" SET country_id = ? WHERE id = ?";
 
     private JdbcUserStorage() {
     }
@@ -120,112 +125,41 @@ public class JdbcUserStorage implements UserStorage {
         return Optional.empty();
     }
 
-    public void updateById(User user) {
-        try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_DATA)) {
-
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getSurname());
-            preparedStatement.setString(3, user.getUsername());
-            preparedStatement.setString(4, Arrays.toString(Base64.getDecoder().decode(user.getPhoto())));
-            preparedStatement.setString(5, user.getEmail());
-            preparedStatement.setString(6, user.getPassword());
-            preparedStatement.setInt(7, user.getId());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void updateUserCountry(int userId, String country) {
-        try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_COUNTRY)) {
-
-            preparedStatement.setString(1, country);
-            preparedStatement.setInt(2, userId);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void updateById(User user) {
-        try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_DATA)) {
-
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getSurname());
-            preparedStatement.setString(3, user.getUsername());
-            preparedStatement.setString(4, Arrays.toString(Base64.getDecoder().decode(user.getPhoto())));
-            preparedStatement.setString(5, user.getEmail());
-            preparedStatement.setString(6, user.getPassword());
-            preparedStatement.setInt(7, user.getId());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void updateUserCountry(int userId, String country) {
-        try (Connection connection = JdbcConnection.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_COUNTRY)) {
-
-            preparedStatement.setString(1, country);
-            preparedStatement.setInt(2, userId);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
     @Override
-   public List<User> getUsersWithUsernameContaining(String keyword){
-        List<User> usersList = new ArrayList<>();
+    public List<User> getUsersWithUsernameContaining(String username) {
+        return null;
+    }
+    @Override
+    public void updateById(User user) {
+        try (Connection connection = JdbcConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_DATA)) {
 
-        try(Connection connection = JdbcConnection.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(GET_BY_USERNAME_CONTAINING);
+            updateUserCountry(user.getId(), user.getCountry().getId());
 
-            preparedStatement.setString(1, keyword + "%");        //preparedStatement.setString(1, "%" + keyword + "%");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getSurname());
+            preparedStatement.setString(3, user.getUsername());
+            preparedStatement.setString(4, Arrays.toString(Base64.getDecoder().decode(user.getPhoto())));
+            preparedStatement.setString(5, user.getEmail());
+            preparedStatement.setString(6, user.getPassword());
+            preparedStatement.setInt(7, user.getId());
 
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()){
-                User user = new User();
-
-                user.setId(resultSet.getInt(1));
-                user.setName(resultSet.getString(2));
-                user.setSurname(resultSet.getString(3));
-                user.setUsername(resultSet.getString(4));
-
-                byte[] image = resultSet.getBytes(5);
-                if(image != null) {
-                user.setPhoto(Base64.getEncoder().encodeToString(resultSet.getBytes(5)));
-                }
-
-                user.setEmail(resultSet.getString(6));
-                user.setPassword(resultSet.getString(7));
-
-                Country country = new Country(
-                        resultSet.getInt(9),
-                        resultSet.getString(10)
-                );
-
-                user.setCountry(country);
-
-                usersList.add(user);
-            }
+            preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return usersList;
+    }
+    public void updateUserCountry(int userId, int countryId) {
+        try (Connection connection = JdbcConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_COUNTRY)) {
 
+            preparedStatement.setInt(1, countryId);
+            preparedStatement.setInt(2, userId);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/by/tms/instaclone22onl/web/servlet/SettingsServlet.java
+++ src/main/java/by/tms/instaclone22onl/web/servlet/SettingsServlet.java
@@ -1,7 +1,12 @@
 package by.tms.instaclone22onl.web.servlet;
 
+import by.tms.instaclone22onl.model.Country;
 import by.tms.instaclone22onl.model.User;
+import by.tms.instaclone22onl.service.CountryService;
+import by.tms.instaclone22onl.service.UserService;
 import by.tms.instaclone22onl.storage.UserStorage.JdbcUserStorage;
+import by.tms.instaclone22onl.storage.UserStorage.UserStorage;
+import by.tms.instaclone22onl.utils.Validator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,51 +14,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+import java.util.List;
+import java.util.Optional;
+
 @WebServlet("/settings")
 public class SettingsServlet extends HttpServlet {
-   private JdbcUserStorage storage;
+    private final UserService userService = UserService.getInstance();
+    private final CountryService countryService = CountryService.getInstance();
+    private final Validator validator = new Validator();
+    private final UserStorage storage = JdbcUserStorage.getInstance();
+    private Country country;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       if (req.getSession().getAttribute("currentUser") == null){
-            resp.sendRedirect("/pages/login.jsp");
-        } else {
-           getServletContext().getRequestDispatcher("/pages/settings.jsp").forward(req, resp);
-       }
+        List<Country> countryList = countryService.getAll();
+        req.setAttribute("countries", countryList);
+        getServletContext().getRequestDispatcher("/pages/settings.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        User currentUser = (User) req.getSession().getAttribute("currentUser");
+        int countryId = Integer.parseInt(req.getParameter("country_id"));
+        country.setId(countryId);
 
         String name = req.getParameter("name");
         String surname = req.getParameter("surname");
         String username = req.getParameter("username");
-        String photo = req.getParameter("photo");
+        InputStream photo = req.getPart("photo").getInputStream();
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         int userId = Integer.parseInt(req.getParameter("id"));
 
-        currentUser.setName(name);
-        currentUser.setSurname(surname);
-        currentUser.setUsername(username);
-        currentUser.setPhoto(photo);
-        currentUser.setUsername(email);
-        currentUser.setPassword(password);
-        currentUser.setId(userId);
+        User user = User.builder()
+                .setName(name)
+                .setSurname(surname)
+                .setUsername(username)
+                .setCountry(country)
+                .setPhoto(Base64.getEncoder().encodeToString(photo.readAllBytes()))
+                .setEmail(email)
+                .setPassword(password)
+                .setId(userId)
+                .build();
 
-       if (req.getSession().getAttribute("currentUser") == null){
-            resp.sendRedirect("/pages/login.jsp");
+
+        Optional<User> byUsername = userService.getUserByName(username);
+
+       if (byUsername.isEmpty()){
+           UserService.getInstance().add(user);
+           resp.sendRedirect("/pages/login.jsp");
         } else {
           try {
-
-              storage.updateUserCountry(currentUser.getId(), String.valueOf(currentUser.getCountry()));
-              storage.updateById(currentUser);
-
+              storage.updateById(user);
               resp.sendRedirect("/pages/settings.jsp");
           } catch (IOException e) {
               throw new RuntimeException(e);
           }
+
+           if (!validator.validate(user)) {
+               req.setAttribute("invalid data", 500);
+           }
        }
     }
 }
