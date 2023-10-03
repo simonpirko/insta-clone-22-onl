4507b9efe353779c1cdad96d76d6bdb6b00a5406
Артем <375298702138artiom@gmail.com>
2023-10-03 18:27:11 +0300

    update

--- src/main/java/by/tms/instaclone22onl/web/servlet/ProfileServlet.java
+++ src/main/java/by/tms/instaclone22onl/web/servlet/ProfileServlet.java
@@ -1,24 +1,36 @@
 package by.tms.instaclone22onl.web.servlet;
 
+import by.tms.instaclone22onl.model.Post;
+import by.tms.instaclone22onl.model.User;
+import by.tms.instaclone22onl.service.PostService;
+import by.tms.instaclone22onl.service.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet("/profile")
 public class ProfileServlet extends HttpServlet {
+    private final UserService userService = UserService.getInstance();
+    private final PostService postService = new PostService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String userName = req.getParameter("username");
+        Optional<User> userByName = userService.getUserByName(userName);
 
+        if (userByName.isPresent()){
+            User user = userByName.get();
 
-
-
+            Optional<Post> posts = postService.getPost(user);
+            req.setAttribute("post", posts);
+            req.setAttribute("user", user);
+        }
         getServletContext().getRequestDispatcher("/pages/profile.jsp").forward(req, resp);
-
-
     }
 }
