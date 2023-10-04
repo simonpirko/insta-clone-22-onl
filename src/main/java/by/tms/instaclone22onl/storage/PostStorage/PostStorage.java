c9e86759c0879f7430dce99be26f5674e376067d
Ivan <ivandolgolaptev@gmail.com>
2023-10-04 14:51:49 +0300

    init: add new branch INST-25_Add_a_customization_page
     Conflicts:
    	src/main/java/by/tms/instaclone22onl/web/servlet/SettingsServlet.java
    	src/main/webapp/pages/settings.jsp

---
+++ src/main/java/by/tms/instaclone22onl/storage/PostStorage/PostStorage.java
@@ -0,0 +1,24 @@
+package by.tms.instaclone22onl.storage.PostStorage;
+
+import by.tms.instaclone22onl.model.Post;
+import by.tms.instaclone22onl.model.User;
+
+import java.util.List;
+import java.util.Optional;
+public interface PostStorage {
+
+    void addPost(Post post);
+
+    Optional<Post> getPost(int id);
+
+    Optional<Post> getPost(User user);
+
+    List<Post> getAllPost();
+
+    boolean deletePost(int id);
+
+    boolean deletePost(User user);
+
+    void updatePost(int id, Post newPost);
+
+}
