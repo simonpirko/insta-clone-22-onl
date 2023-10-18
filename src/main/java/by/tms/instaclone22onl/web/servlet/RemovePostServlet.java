71dd5014cf5a1bdce10279f3261152444f2a2b8a
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-10-18 14:50:09 +0300

    New request for removing post was created, RemovePostServlet was created

---
+++ src/main/java/by/tms/instaclone22onl/web/servlet/RemovePostServlet.java
@@ -0,0 +1,28 @@
+package by.tms.instaclone22onl.web.servlet;
+
+import by.tms.instaclone22onl.entity.User;
+import by.tms.instaclone22onl.service.PostService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/remove_post")
+public class RemovePostServlet extends HttpServlet {
+   PostService postService = PostService.getInstance();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) req.getSession().getAttribute("currentUser");
+        int postId = Integer.parseInt(req.getParameter("postId"));
+
+        if(postService.findById(postId) != null){
+            postService.removeById(postId);
+        }
+
+        resp.sendRedirect("/user/viewpost");
+
+    }
+}
